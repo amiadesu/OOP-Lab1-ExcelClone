273e8698922f37607ff1e6971042b9549fd38571
Amia <hermionegranger3423@gmail.com>
2025-10-11 15:58:58 +0300

    Added Google Drive service and Secrets.cs.example (where client ID should be stored)

--- Source/Services/Files/TableFileService.cs
+++ Source/Services/Files/TableFileService.cs
@@ -10,12 +10,13 @@ using ExcelClone.Components;
 using ExcelClone.Resources.Localization;
 using ExcelClone.Services;
 using ExcelClone.Utils;
+using ExcelClone.Services.GoogleDrive;
 
 namespace ExcelClone.FileSystem;
 
 public static class TableFileService
 {
-    public static async Task<string> Save(ICellStorageReader spreadsheet, string fileName = "result.table")
+    public static string ConstructFileFromCellStorage(ICellStorageReader spreadsheet)
     {
         StringBuilder result = new StringBuilder();
 
@@ -36,7 +37,14 @@ public static class TableFileService
             result.AppendLine(formula);
         }
 
-        var fileSaverResult = await PickAndSaveFile(result.ToString(), fileName);
+        return result.ToString();
+    }
+
+    public static async Task<string> Save(ICellStorageReader spreadsheet, string fileName = "result.table")
+    {
+        var content = ConstructFileFromCellStorage(spreadsheet);
+
+        var fileSaverResult = await PickAndSaveFile(content, fileName);
         if (fileSaverResult.IsSuccessful)
         {
             return DataProcessor.FormatResource(
@@ -50,10 +58,62 @@ public static class TableFileService
         );
     }
 
-    public static Spreadsheet Load(string path, ICellNameService nameService)
+    public static async Task<string> SaveToGoogleDrive(ICellStorageReader spreadsheet,
+        IGoogleDriveService googleDriveService, string fileName = "result.table")
     {
+        try
+        {
+            var content = ConstructFileFromCellStorage(spreadsheet);
+
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+
+            var file = await googleDriveService.UploadFileAsync(fileName, stream, "text/plain");
+
+            return DataProcessor.FormatResource(
+                AppResources.FileSavedSuccessfully,
+                ("Path", $"Google Drive, {file.Name} (id: {file.Id})")
+            );
+        }
+        catch (Exception e)
+        {
+            return DataProcessor.FormatResource(
+                AppResources.FileSavingError,
+                ("Error", e.Message)
+            );
+        }
+    }
+
+    public static Spreadsheet LoadFromPath(string path, ICellNameService nameService)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+            throw new ArgumentException("Path cannot be null or empty", nameof(path));
+        if (!File.Exists(path))
+            throw new FileNotFoundException("File not found", path);
+
         using var reader = new StreamReader(path);
 
+        return LoadFile(reader, nameService);
+    }
+
+    public static Spreadsheet LoadFromContentString(string content, ICellNameService nameService)
+    {
+        using var reader = new StreamReader(
+            new MemoryStream(Encoding.UTF8.GetBytes(content))
+        );
+
+        return LoadFile(reader, nameService);
+    }
+
+    public static async Task<Spreadsheet> LoadFromGoogleDrive(string fileId,
+        IGoogleDriveService googleDriveService, ICellNameService nameService)
+    {
+        var content = await googleDriveService.DownloadFileAsync(fileId);
+
+        return LoadFromContentString(content, nameService);
+    }
+
+    private static Spreadsheet LoadFile(StreamReader reader, ICellNameService nameService)
+    {
         // Read header
         string? header = reader.ReadLine();
         if (header == null)
@@ -118,7 +178,7 @@ public static class TableFileService
         try
         {
             var result = await FilePicker.Default.PickAsync(options);
-            
+
             return (result, "");
         }
         catch (Exception ex)
---
+++ Source/Services/GoogleDrive/IGoogleDriveService.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ExcelClone.Services.GoogleDrive;
+
+public interface IGoogleDriveService
+{
+    public bool IsSignedIn { get; }
+    public string? Email { get; }
+
+    public Task Init();
+    public Task SignIn();
+    public Task<List<Google.Apis.Drive.v3.Data.File>> GetFiles(string? fileExtension = null);
+    public Task<string> DownloadFileAsync(string fileId);
+    public Task<Google.Apis.Drive.v3.Data.File> UploadFileAsync(string fileName, Stream content, string mimeType);
+    public Task SignOut();
+}
